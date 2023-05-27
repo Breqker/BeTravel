14745535d2a5cb70ad825ecd5382bedc5b80befe
FrancescoLindiner <fralindi01@gmail.com>
2023-05-27 09:00:42 +0200

    Navigation bar

--- app/src/main/java/com/example/betravel/Home.kt
+++ app/src/main/java/com/example/betravel/Home.kt
@@ -2,15 +2,56 @@ package com.example.betravel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.betravel.databinding.ActivityHomeBinding
+import com.google.android.material.navigation.NavigationView
 
-class Home : AppCompatActivity() {
+class Home : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        drawerLayout = binding.drawerLayout
+
+        setSupportActionBar(binding.toolbar)
+
+        val navigationView = binding.navView
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, binding.toolbar, R.string.open_nav, R.string.close_nav)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, VisualizzaDatiFragment()).commit()
+            navigationView.setCheckedItem(R.id.nav_visualizza_dati)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_visualizza_dati -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container, VisualizzaDatiFragment()).commit()
+            R.id.nav_modifica_dati -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container, ModificaDatiFragment()).commit()
+            R.id.nav_logout -> Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show()
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            onBackPressedDispatcher.onBackPressed()
+        }
     }
 }
