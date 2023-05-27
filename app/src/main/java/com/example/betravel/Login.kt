6546e490d54879fd551de8962fe8099fd01ccf83
Breqker <gia.ferrara27@gmail.com>
2023-05-27 14:49:19 +0200

    Registrazione e Login gestiti anche orizzontalmente

--- app/src/main/java/com/example/betravel/Login.kt
+++ app/src/main/java/com/example/betravel/Login.kt
@@ -2,24 +2,30 @@ package com.example.betravel
 
 import android.content.DialogInterface
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.betravel.databinding.ActivityLoginBinding
-
+import com.example.betravel.databinding.ActivityLoginLandBinding
 
 class Login : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var bindingLand: ActivityLoginLandBinding
     private lateinit var dbHelper: DBHelper
     private lateinit var dbManager: DBManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        bindingLand = ActivityLoginLandBinding.inflate(layoutInflater)
+
+        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(bindingLand.root)
+        } else if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            setContentView(binding.root)
+        }
 
         dbHelper = DBHelper(this)
         dbManager = DBManager(dbHelper)
--- app/src/main/java/com/example/betravel/Registrazione.kt
+++ app/src/main/java/com/example/betravel/Registrazione.kt
@@ -2,21 +2,30 @@ package com.example.betravel
 
 import android.content.DialogInterface
 import android.content.Intent
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import com.example.betravel.databinding.ActivityRegistrazioneBinding
+import com.example.betravel.databinding.ActivityRegistrazioneLandBinding
 
 class Registrazione : AppCompatActivity() {
 
     private lateinit var binding : ActivityRegistrazioneBinding
+    private lateinit var bindingLand : ActivityRegistrazioneLandBinding
     private lateinit var dbHelper: DBHelper
     private lateinit var dbManager: DBManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegistrazioneBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        bindingLand = ActivityRegistrazioneLandBinding.inflate(layoutInflater)
+
+        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(bindingLand.root)
+        } else if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            setContentView(binding.root)
+        }
 
         dbHelper = DBHelper(this)
         dbManager = DBManager(dbHelper)
