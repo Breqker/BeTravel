33dc15a26694f1ef2b7deefdea6a3a43a2c2337d
FrancescoLindiner <fralindi01@gmail.com>
2023-05-28 23:21:23 +0200

    Home orizzontale

--- app/src/main/java/com/example/betravel/Home.kt
+++ app/src/main/java/com/example/betravel/Home.kt
@@ -1,43 +1,75 @@
 package com.example.betravel
 
+import android.content.res.Configuration
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.betravel.databinding.ActivityLoginBinding
+import com.example.betravel.databinding.ActivityLoginLandBinding
 import com.example.betravel.databinding.ActivityMainBinding
+import com.example.betravel.databinding.ActivityMainOrizzontaleBinding
 
-class Home: AppCompatActivity() {
+class Home : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var bindingOrizzontale: ActivityMainOrizzontaleBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.recyclerview1.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
-        val data = ArrayList<ItemsViewModelCategorie>()
+        bindingOrizzontale = ActivityMainOrizzontaleBinding.inflate(layoutInflater)
 
-        data.add(ItemsViewModelCategorie(R.drawable.volo, "Scopri tutti i voli"))
-        data.add(ItemsViewModelCategorie(R.drawable.soggiorno, "Soggiorno"))
-        data.add(ItemsViewModelCategorie(R.drawable.taxi, "Prenota taxi"))
-        data.add(ItemsViewModelCategorie(R.drawable.crociera, "Scopri le crociere"))
+        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(bindingOrizzontale.root)
+            bindingOrizzontale.recyclerview1.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+            val data = ArrayList<ItemsViewModelCategorie>()
 
-        val adapter1 = CustomAdapter(data)
-        binding.recyclerview1.adapter = adapter1
+            data.add(ItemsViewModelCategorie(R.drawable.volo, "Scopri tutti i voli"))
+            data.add(ItemsViewModelCategorie(R.drawable.soggiorno, "Soggiorno"))
+            data.add(ItemsViewModelCategorie(R.drawable.taxi, "Prenota taxi"))
+            data.add(ItemsViewModelCategorie(R.drawable.crociera, "Scopri le crociere"))
+
+            val adapter1 = CustomAdapter(data)
+            bindingOrizzontale.recyclerview1.adapter = adapter1
+
+            bindingOrizzontale.recyclerview2.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+            val data2 = ArrayList<ItemsViewModelPacchetti>()
+
+            data2.add(ItemsViewModelPacchetti(R.drawable.pacchetto_famiglia, "Scopri tutti i voli"))
+            data2.add(ItemsViewModelPacchetti(R.drawable.pacchetto_famiglia, "Soggiorno"))
+            data2.add(ItemsViewModelPacchetti(R.drawable.pacchetto_famiglia, "Prenota taxi"))
+            data2.add(ItemsViewModelPacchetti(R.drawable.pacchetto_famiglia, "Scopri le crociere"))
+
+            val adapter2 = CustomAdapterPacchetti(data2)
+            bindingOrizzontale.recyclerview2.adapter = adapter2
+
+        } else if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            setContentView(binding.root)
+            binding.recyclerview1.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+            val data = ArrayList<ItemsViewModelCategorie>()
+
+            data.add(ItemsViewModelCategorie(R.drawable.volo, "Scopri tutti i voli"))
+            data.add(ItemsViewModelCategorie(R.drawable.soggiorno, "Soggiorno"))
+            data.add(ItemsViewModelCategorie(R.drawable.taxi, "Prenota taxi"))
+            data.add(ItemsViewModelCategorie(R.drawable.crociera, "Scopri le crociere"))
+
+            val adapter1 = CustomAdapter(data)
+            binding.recyclerview1.adapter = adapter1
+
+            binding.recyclerview2.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+            val data2 = ArrayList<ItemsViewModelPacchetti>()
+
+            data2.add(ItemsViewModelPacchetti(R.drawable.pacchetto_famiglia, "Scopri tutti i voli"))
+            data2.add(ItemsViewModelPacchetti(R.drawable.pacchetto_famiglia, "Soggiorno"))
+            data2.add(ItemsViewModelPacchetti(R.drawable.pacchetto_famiglia, "Prenota taxi"))
+            data2.add(ItemsViewModelPacchetti(R.drawable.pacchetto_famiglia, "Scopri le crociere"))
+
+            val adapter2 = CustomAdapterPacchetti(data2)
+            binding.recyclerview2.adapter = adapter2
+        }
 
 
-
-
-        binding.recyclerview2.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
-        val data2 = ArrayList<ItemsViewModelPacchetti>()
-
-        data2.add(ItemsViewModelPacchetti(R.drawable.pacchetto_famiglia, "Scopri tutti i voli"))
-        data2.add(ItemsViewModelPacchetti(R.drawable.pacchetto_famiglia, "Soggiorno"))
-        data2.add(ItemsViewModelPacchetti(R.drawable.pacchetto_famiglia, "Prenota taxi"))
-        data2.add(ItemsViewModelPacchetti(R.drawable.pacchetto_famiglia, "Scopri le crociere"))
-
-        val adapter2 = CustomAdapterPacchetti(data2)
-        binding.recyclerview2.adapter = adapter2
     }
 }
