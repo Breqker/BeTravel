1d039dfe5f3c95e40fce338639252fa570c9285b
Giacomo Ferrara <giacomo.ferrara@community.unipa.it>
2023-05-29 19:31:03 +0200

    Layout verticale e orizzontale Volo

--- app/src/main/java/com/example/betravel/FragmentVolo.kt
+++ app/src/main/java/com/example/betravel/FragmentVolo.kt
@@ -1,23 +1,136 @@
 package com.example.betravel
 
+import android.app.DatePickerDialog
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import com.example.betravel.databinding.FragmentVoloBinding
+import com.example.betravel.databinding.FragmentVoloLandBinding
+import java.util.*
 
 class FragmentVolo : Fragment() {
 
     private lateinit var binding: FragmentVoloBinding
+    private lateinit var bindingLand: FragmentVoloLandBinding
+    private val calendar = Calendar.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentVoloBinding.inflate(inflater,container,false)
-        val view = binding.root
-        return view
+    ): View {
+        val orientation = resources.configuration.orientation
+
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            bindingLand = FragmentVoloLandBinding.inflate(inflater, container, false)
+            val view = bindingLand.root
+
+            bindingLand.dataPartenza.isFocusable = false
+            bindingLand.dataPartenza.isClickable = true
+
+            bindingLand.dataPartenza.setOnClickListener {
+                showDatePicker(bindingLand.dataPartenza)
+            }
+
+            bindingLand.dataArrivo.isFocusable = false
+            bindingLand.dataArrivo.isClickable = true
+
+            bindingLand.dataArrivo.setOnClickListener {
+                showDatePicker(bindingLand.dataArrivo)
+            }
+
+            val numbers = ArrayList<String>()
+            for (i in 1..10) {
+                numbers.add(i.toString())
+            }
+
+            bindingLand.numPersone.isFocusable = false
+            bindingLand.numPersone.isClickable = false
+
+            val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, numbers)
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+            val numPersoneSpinner: Spinner = bindingLand.numPersoneSpinner
+            numPersoneSpinner.adapter = adapter
+
+            bindingLand.barraRicerca.isFocusable = false
+            bindingLand.barraRicerca.isClickable = true
+
+            bindingLand.barraRicerca.setOnClickListener {
+
+            }
+
+            return view
+        } else {
+            binding = FragmentVoloBinding.inflate(inflater, container, false)
+            val view = binding.root
+
+            binding.dataPartenza.isFocusable = false
+            binding.dataPartenza.isClickable = true
+
+            binding.dataPartenza.setOnClickListener {
+                showDatePicker(binding.dataPartenza)
+            }
+
+            binding.dataArrivo.isFocusable = false
+            binding.dataArrivo.isClickable = true
+
+            binding.dataArrivo.setOnClickListener {
+                showDatePicker(binding.dataArrivo)
+            }
+
+            val numbers = ArrayList<String>()
+            for (i in 1..10) {
+                numbers.add(i.toString())
+            }
+
+            binding.numPersone.isFocusable = false
+            binding.numPersone.isClickable = false
+
+            val adapter =
+                ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, numbers)
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+            val numPersoneSpinner: Spinner = binding.numPersoneSpinner
+            numPersoneSpinner.adapter = adapter
+
+            binding.barraRicerca.isFocusable = false
+            binding.barraRicerca.isClickable = true
+
+            binding.barraRicerca.setOnClickListener {
+
+            }
+
+            return view
+        }
+    }
+
+    private fun showDatePicker(editText: EditText) {
+        val datePickerDialog = DatePickerDialog(
+            requireContext(),
+            { _, year, monthOfYear, dayOfMonth ->
+                val selectedDate = formatDate(year, monthOfYear, dayOfMonth)
+                editText.setText(selectedDate)
+            },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+
+        datePickerDialog.datePicker.minDate = System.currentTimeMillis() - 1000
+
+        datePickerDialog.show()
+    }
+
+    private fun formatDate(year: Int, month: Int, day: Int): String {
+        val formattedMonth = if (month + 1 < 10) "0${month + 1}" else "${month + 1}"
+        val formattedDay = if (day < 10) "0$day" else "$day"
+        return "$formattedDay/$formattedMonth/$year"
     }
 }
