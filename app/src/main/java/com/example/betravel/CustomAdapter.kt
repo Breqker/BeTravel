b55ec63f93fd0f6acb4dd8b1c360756b943b9a9e
Breqker <gia.ferrara27@gmail.com>
2023-05-29 10:09:39 +0200

    Aggiustamenti vari

--- app/src/main/java/com/example/betravel/CustomAdapter.kt
+++ app/src/main/java/com/example/betravel/CustomAdapter.kt
@@ -3,16 +3,16 @@ package com.example.betravel
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.betravel.databinding.RecentsRowItemBinding
+import com.example.betravel.databinding.PacchettiRowItemsBinding
 
 class CustomAdapter(private val myList: List<ItemsViewModelCategorie>) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
-    class ViewHolder(binding: RecentsRowItemBinding): RecyclerView.ViewHolder(binding.root){
-        val imageView = binding.imageCategoria
-        val textView = binding.categoria
+    class ViewHolder(binding: PacchettiRowItemsBinding): RecyclerView.ViewHolder(binding.root){
+        val imageView = binding.imagePacchetto
+        val textView = binding.categoriaPacchetto
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = RecentsRowItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val view = PacchettiRowItemsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(view)
     }
 
