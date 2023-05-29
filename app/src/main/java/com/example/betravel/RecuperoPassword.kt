53a968c51a7ac92d3fced88ac8fa046f66fad9c0
Breqker <gia.ferrara27@gmail.com>
2023-05-29 11:46:56 +0200

    Implementato recupero password , da controllare il funzionamento

--- app/src/main/java/com/example/betravel/RecuperoPassword.kt
+++ app/src/main/java/com/example/betravel/RecuperoPassword.kt
@@ -1,6 +1,8 @@
 package com.example.betravel
 
+import android.content.Intent
 import android.content.res.Configuration
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.betravel.databinding.ActivityRecuperoPasswordBinding
@@ -20,6 +22,28 @@ class RecuperoPassword : AppCompatActivity() {
             setContentView(bindingLand.root)
         } else if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
             setContentView(binding.root)
+        }
+
+        binding.recuperoPassword.setOnClickListener{
+            val email = binding.editRecuperoPassword.text.toString()
+            sendEmail(email)
+        }
+
+        bindingLand.recuperoPassword.setOnClickListener{
+            val email = bindingLand.editRecuperoPassword.text.toString()
+            sendEmail(email)
+        }
+    }
+
+    private fun sendEmail(email: String) {
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:$email")
+            putExtra(Intent.EXTRA_SUBJECT, "Recupero Password")
+            putExtra(Intent.EXTRA_TEXT, "Ciao,\n\nAbbiamo ricevuto la tua richiesta di recupero password.\n\nInviati ulteriori dettagli per il recupero.\n\nCordiali saluti,\nIl tuo team di supporto")
+        }
+
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
         }
     }
 }
