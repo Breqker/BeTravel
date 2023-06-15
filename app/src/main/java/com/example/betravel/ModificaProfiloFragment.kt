29ee059008dbe21af8be79f0def188002f4fa508
FrancescoLindiner <fralindi01@gmail.com>
2023-06-15 09:45:01 +0200

    ModificaProfiloFragment: adesso è possibile modificare solo un campo o più campi

--- app/src/main/java/com/example/betravel/ModificaProfiloFragment.kt
+++ app/src/main/java/com/example/betravel/ModificaProfiloFragment.kt
@@ -3,6 +3,7 @@ package com.example.betravel
 import android.content.DialogInterface
 import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -51,10 +52,10 @@ class ModificaProfiloFragment : Fragment(){
             val view = binding.root
 
             binding.saveButton.setOnClickListener {
-                val nome = binding.editName.text.toString()
-                val cognome = binding.editCognome.text.toString()
-                val email = binding.editEmail.text.toString()
-                val password = binding.editPassword.text.toString()
+                var nome = binding.editName.text.toString()
+                var cognome = binding.editCognome.text.toString()
+                var email = binding.editEmail.text.toString()
+                var password = binding.editPassword.text.toString()
 
                 modificaDati(nome,cognome, email, password)
             }
@@ -83,34 +84,35 @@ class ModificaProfiloFragment : Fragment(){
 
     private fun modificaDati(nome: String,cognome: String,email: String,password: String){
         val id = Utente.getId()
+        val updateFields = mutableListOf<String>()
 
-        val updateQuery = "UPDATE Utente\n" +
-                "SET \n" +
-                "    nome = COALESCE('$nome', nome),\n" +
-                "    cognome = COALESCE('$cognome', cognome),\n" +
-                "    email = COALESCE('$email', email),\n" +
-                "    password = COALESCE('$password', password)\n" +
-                "WHERE id = '$id'; "
+        if (!nome.isEmpty()) {
+            updateFields.add("nome = '$nome'")
+        }
+        if (!cognome.isEmpty()) {
+            updateFields.add("cognome = '$cognome'")
+        }
+        if (!email.isEmpty()) {
+            updateFields.add("email = '$email'")
+        }
+        if (!password.isEmpty()) {
+            updateFields.add("password = '$password'")
+        }
+
+        val updateQuery = "UPDATE Utente SET ${updateFields.joinToString(", ")} WHERE id = '$id';"
 
         val updateCall = ClientNetwork.retrofit.update(updateQuery)
         updateCall.enqueue(object : Callback<JsonObject> {
             override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
                 if (response.isSuccessful) {
-                    val data = response.body()?.get("queryset") as JsonArray
-
-                    if(data.size() > 0){
-                        showMessageConferma("Sei sicuro delle modifiche effettuate ?")
-                        showMessage("Modifica avvenuta con successo")
-                    }else{
-                        showMessage("Errore durante la modifica")
-                    }
+                    showMessage("Modifica avvenuta con successo")
                 } else {
-                    showMessage("Risposta dal server vuota")
+                    showMessage("Risposta dal server non riuscita")
                 }
             }
 
             override fun onFailure(call: Call<JsonObject>, t: Throwable) {
-                showMessage("Errore di connessione: ${t.message}")
+                TODO("Not yet implemented")
             }
         })
     }
